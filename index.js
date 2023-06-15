6b49fa7de31be0b3d198c679c721abae4ca1092b
Adri Antori <adriantori11@gmail.com>
2023-06-15 23:05:06 +0700

    changes layout and background, adding javascript functionality

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function hello(){
-    alert("Hello, Adri!");
+let modal = document.getElementById("opening");
+
+let span = document.getElementsByClassName("close")[0];
+
+let visitor = prompt("who arth thou?");
+
+if (visitor == "") {
+    visitor = "Anon"
+    document.getElementById("visitor").innerText = "Anon";
+}else {
+    document.getElementById("visitor").innerText = visitor;
 }
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+function hello(){
+    alert("Hello, " + visitor);
+}
+
